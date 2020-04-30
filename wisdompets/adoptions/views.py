2aab88ca09228d3bb7f6ce9134f07798be7a6940
Steve Sultan <stevesultan@outlook.com>
2020-04-30 16:50:20 -0400

    Revert "Added CSS and JS"
    This reverts commit b1b9059b4cca46a26980f8cd285f0e41dee596a3.

--- wisdompets/adoptions/views.py
+++ wisdompets/adoptions/views.py
@@ -1,20 +1,3 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from.models import Pet
-from django.http import Http404
 
 # Create your views here.
-def home(request):
-    pets = Pet.objects.all()
-    return render(request,'home.html', {'pets':pets})
-    #return HttpResponse('<p>home view</p>')
-
-def pet_detail(request,id):
-    try:
-        pet = Pet.objects.get(id=id)
-    except Pet.DoesNotExist:
-        raise Http404('Pet not found')
-    return render(request,'pet_detail.html', {'pet': pet})
-    #return HttpResponse('<p>pet_detail view with the id {}</p>'.format(id))
-
-
--- wisdompets/wisdompets/urls.py
+++ wisdompets/wisdompets/urls.py
@@ -13,14 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-
-from django.conf.urls import url
 from django.contrib import admin
-from adoptions import views
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$',views.home, name = 'home'),
-    url(r'^adoptions/(\d+)/', views.pet_detail, name='pet_detail')
+from django.urls import path
 
+urlpatterns = [
+    path('admin/', admin.site.urls),
 ]
